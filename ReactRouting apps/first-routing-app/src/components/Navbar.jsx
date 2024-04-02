ee69e7a8d90103c5e98108190c590cbab54dcade
Joshua Curry <jocurry2017@outlook.com>
2024-04-02 14:59:46 -0500

    added content to first-routing-app which included using NavLinks and other data

--- ReactRouting apps/first-routing-app/src/components/Navbar.jsx
+++ ReactRouting apps/first-routing-app/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div>
-        
-    </div>
+    <ul>
+      <li>
+        <NavLink to ="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to ="/instructors">Instructors</NavLink>
+      </li>
+      <li>
+        <NavLink to ="/contact">Contact</NavLink>
+      </li>
+    </ul>
   )
 }
 
